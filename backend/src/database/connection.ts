f328faeb34e95ade9b78a93f8efdefbbcadaddca
ancaD <anca_dr10@yahoo.com>
2025-01-20 22:28:38 +0200

    connection changes

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,34 +1,13 @@
-import { Dialect } from "sequelize";
+
 import { Sequelize } from "sequelize-typescript";
 import 'dotenv/config';
 
-function getEnvVar(key: string): string {
-    const value = process.env[key];
-    if (!value) {
-        throw new Error(`Missing environment variable: ${key}`);
-    }
-    return value;
-}
-
-const dialect: Dialect = process.env.DB_DIALECT as Dialect;
-if (!dialect) {
-    throw new Error("Invalid or missing DB_DIALECT in .env");
-}
-
-const host = getEnvVar("DB_HOST");
-const username = getEnvVar("DB_USER");
-const password = getEnvVar("DB_PASS");
-const database = getEnvVar("DB_NAME");
-const port = Number(process.env.DB_PORT) || 3306;
-const models = [__dirname + '/../models'];
-
-// Configurarea Sequelize
 export const connection = new Sequelize({
-    dialect,
-    host,
-    username,
-    password,
-    database,
-    port,
-    models
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT as any || 3306,
+    models: [__dirname + '/../models']
 });
